bdd9bba528825ae556d8ae9b6529009712619777
Godwin Augustine <godwinaugustine118@gmail.com>
2024-08-07 15:17:25 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,32 @@
+// change navbar style on scroll
+
+window.addEventListener("scroll", () =>{
+    document.querySelector("nav").classList.toggle
+    ("window-scroll", window.scrollY > 0)
+})
+
+// show/hide nav menu
+const menu = document.querySelector(".nav__menu");
+const menubtn = document.querySelector("#open-menu-btn");
+const closebtn = document.querySelector("#close-menu-btn");
+
+
+menubtn.addEventListener('click', () => {
+    menu.style.display = "flex";
+    closebtn.style.display = "inline-block";
+    menubtn.style.display = "none";
+})
+
+
+// close nav menu
+const closeNav = () => {
+    menu.style.display = "none";
+    closebtn.style.display = "none";
+    menubtn.style.display = "inline-block";
+}
+
+closebtn.addEventListener('click', closeNav)
+
+
+
+
